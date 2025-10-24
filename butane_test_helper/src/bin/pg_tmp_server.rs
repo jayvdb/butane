59912a6671a246f7a4203bfe7dac32797f61bbe9
John Vandenberg <jayvdb@gmail.com>
2025-10-24 09:41:29 +0800

    Improve pg_tmp_server

--- butane_test_helper/src/bin/pg_tmp_server.rs
+++ butane_test_helper/src/bin/pg_tmp_server.rs
@@ -1,6 +1,7 @@
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 
+use butane_core::db::{connect, ConnectionSpec};
 use butane_test_helper::{pg_tmp_server_create, PgServerOptions};
 
 fn main() {
@@ -12,9 +13,22 @@ fn main() {
     .unwrap();
     // Print the connection string
     println!(
-        "Running temporary PostgreSQL server\nUnix socket dir: {}\nUser: postgres",
+        "Running temporary PostgreSQL server\nUnix socket dir: {}\nUser: postgres\nPort: 5432",
         server.sockdir.path().display()
     );
+
+    let connection_spec = ConnectionSpec::new("pg", format!(
+        "host={} user=postgres port=5432",
+        server.sockdir.path().display()
+    ));
+
+    let new_dbname = format!("butane_tmp_{}", uuid::Uuid::new_v4().simple());
+
+    let conn = connect(&connection_spec).unwrap();
+    conn.execute(format!("CREATE DATABASE {new_dbname};"))
+        .unwrap();
+
+    println!("Temporary database created: {new_dbname}");
 
     let running = Arc::new(AtomicBool::new(true));
     let r = running.clone();
